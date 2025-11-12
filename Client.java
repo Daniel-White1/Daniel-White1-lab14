fc6a5c1318e6c31aae4ec615f1b1c679d7f7a30b
Daniel White <daniel.white1@gwmail.gwu.edu>
2025-11-12 16:28:41 -0500

    Created empty framework for all manditory methods

--- Client.java
+++ Client.java
@@ -1,3 +1,31 @@
+import java.net.Socket;
+
 public class Client {
+    //variables to store the socket of a client
+    Socket sock;
+    public Client (String host, int port){
+        try{
+        this.sock = new Socket(host, port);
+        } catch (Exception e){
+            System.err.println("Couldn't connect");
+            System.exit(1);
+        }
+    }
+
+    //Simple method to get the Socket
+    public Socket getSocket(){
+        return this.sock;
+    }
+
+    public void handshake() {
+
+    }
+
+    public void request(){
     
+    }
+
+    public void disconnect(){
+        
+    }
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,24 @@
-public class Server {
-    
+import java.net.Socket;
+
+public class Server{
+    public Socket sock;
+    public Server(Socket scok){
+        this.sock = sock;
+    }
+
+    public void serve(){
+
+    }
+
+    public void run(){
+
+    }
+
+    public void disconnect(){
+
+    }
+
+    public void getConnectedTimes(){
+
+    }
 }
